387bbd1e235611a05442006794bd9e8666c904f5
Dachi Darjania <dachidar19@gmail.com>
2026-03-27 16:13:03 +0400

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+// CommonJS, Every file is module (by default)
+// Modules  - Encapsuldated Code (only share minimum)
+
+const {john, peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require("./6-alternative-flavor")
+
+require("./7-mind-granade")
+
+// sayHi("Susan")
+// sayHi(john)
+// sayHi(peter)
