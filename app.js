2ed23ce27000a1b99fd06ae5d0034f4d754938f3
Dachi Darjania <dachidar19@gmail.com>
2026-03-28 17:36:53 +0400

    second commit

--- app.js
+++ app.js
@@ -13,8 +13,8 @@
 // or npm init -y (everything default)
 
 
-const _ = require('lodash');
+const _ = require("lodash")
 
-const items = [1,[2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems);
